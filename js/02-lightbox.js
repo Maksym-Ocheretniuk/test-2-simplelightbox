8cbd34cb98f0cc9c8ffc22fdcb6fa904398ad6cf
Maksym Ocheretniuk <max.ocheret@gmail.com>
2023-03-17 23:22:56 +0200

    task 07-02 update
    прибирання подвійного кліку

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -9,9 +9,6 @@ const galleryEl = document.querySelector(".gallery");
 // Додаємо розмітку в структуру
 galleryEl.innerHTML = createListImg(galleryItems);
 
-// Вішаємо слухача подій (клік на "li") на список "ul" (вспливання події)
-galleryEl.addEventListener("click", onGalleryElementClick);
-
 // Функція створення розмітки "li" списку в HTML
 function createListImg(images) {
   return images
@@ -30,24 +27,12 @@ function createListImg(images) {
     .join("");
 }
 
-function onGalleryElementClick(evt) {
-  // заборона переходу за посиланням
-  evt.preventDefault();
-
-  // Перевірка, щоб клік був на зображенні
-  if (evt.target.nodeName !== "IMG") {
-    return;
-  }
-
-  // Отримання url великого зображення
-  const largeImageUrl = evt.target.dataset.source;
-
-  const gallery = new SimpleLightbox(".gallery a", {
-    // captions: true,
-    captionsData: "alt",
-    captionDelay: 250,
-    // nav: true,
-    // close: true,
-    // overlay: true,
-  });
-}
+// бібліотека SimpleLightbox
+const lightbox = new SimpleLightbox(".gallery a", {
+  // captions: true,
+  captionsData: "alt",
+  captionDelay: 250,
+  // nav: true,
+  // close: true,
+  // overlay: true,
+});
